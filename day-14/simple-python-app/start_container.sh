28816b596bc3c3aea1701b2394130c81fdcd851b
varshini1996 <111688716+varshini1996@users.noreply.github.com>
2023-07-19 12:57:27 +0530

    add the files for code deploy

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull varshini1996/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 varshini1996/simple-python-flask-app
