da88b94d0427f2c5761c8f0b473cdfcdd463a20f
varshini1996 <111688716+varshini1996@users.noreply.github.com>
2023-07-20 14:31:15 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,4 +4,4 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull varshini1996/simple-python-flask-app
 # Run the Docker image as a container
-docker run -d -p 5000:5000 varshini1996/simple-python-flask-app 
+docker run -d -p 5000:5000 varshini1996/simple-python-flask-app
